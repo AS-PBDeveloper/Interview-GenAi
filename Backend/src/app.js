56394b03efd1df5dc7dc66b077aa366eac70052b
Angkita Paul <paulankita614@gmail.com>
2026-03-11 13:15:49 +0530

    UP

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
